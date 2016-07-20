ca3ea8e0700fe4408016525ca3384815cc0f7576
ATOM27 <gek2395@mail.ru>
2016-07-21 01:02:55 +0300

    start to learn UITextField

---
+++ ObjCUITextFieldPart1/ObjCUITextFieldPart1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ObjCUITextFieldPart1
+//
+//  Created by Eugene Mekhedov on 20.07.16.
+//  Copyright © 2016 Eugene Mekhedov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ObjCUITextFieldPart1/ObjCUITextFieldPart1/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  ObjCUITextFieldPart1
+//
+//  Created by Eugene Mekhedov on 20.07.16.
+//  Copyright © 2016 Eugene Mekhedov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITextField *firstNameField;
+@property (weak, nonatomic) IBOutlet UITextField *secondNameField;
+
+
+@end
+
