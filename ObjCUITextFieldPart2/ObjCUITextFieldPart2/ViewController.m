7242301005802702207d6fc1f3e2e4cabffddc2c
ATOM27 <gek2395@mail.ru>
2016-07-24 17:14:27 +0300

    part 2 complete

--- ObjCUITextFieldPart2/ObjCUITextFieldPart2/ViewController.m
+++ ObjCUITextFieldPart2/ObjCUITextFieldPart2/ViewController.m
@@ -49,13 +49,60 @@
     static const int areaCodeMaxLenght = 3;
     static const int countryCodeMaxLenght = 3;
     
+    NSArray* clearNumber = [newString componentsSeparatedByCharactersInSet:digitsSetInverted];
+    newString = [clearNumber componentsJoinedByString:@""];
+    
+    if ([newString length] > localNumberMaxLenght + areaCodeMaxLenght + countryCodeMaxLenght){
+        return FALSE;
+    }
+    
+    // +XX (XXX) XXX-XXXX
+    
+    NSInteger localNumberLenght = MIN([newString length], localNumberMaxLenght);
+    
+    if (localNumberLenght > 0){
+        
+        NSString* numberString = [newString substringFromIndex:[newString length] - localNumberLenght];
+        
+        [resultString appendString:numberString];
+        
+        if ([resultString length] > 3){
+            [resultString insertString:@"-" atIndex:3];
+        }
+    }
+    
+    if ([newString length] > localNumberMaxLenght){
+        
+        NSInteger areaLenght = MIN([newString length] - localNumberMaxLenght, areaCodeMaxLenght);
+        
+        NSRange range = NSMakeRange((int)[newString length] - localNumberMaxLenght - areaLenght, areaLenght);
+        
+        NSString* areaString = [newString substringWithRange:range];
+        
+        areaString = [NSString stringWithFormat:@"(%@) ", areaString];
+        
+        [resultString insertString:areaString atIndex:0];
+        
+    }
+    
+    if ([newString length] > localNumberMaxLenght + areaCodeMaxLenght){
+        
+        NSString* countryString = [newString substringToIndex:[newString length] - localNumberMaxLenght - areaCodeMaxLenght];
+        countryString = [NSString stringWithFormat:@"+%@ ", countryString];
+        
+        [resultString insertString:countryString atIndex:0];
+    }
+    
+    
 //    NSCharacterSet* set = [NSCharacterSet characterSetWithCharactersInString:@" ,"];// if you want to create own separators
 //    NSCharacterSet* digitsOnly = [[NSCharacterSet decimalDigitCharacterSet] invertedSet];//inverted set means - all not including decimalDigitCharacterSet
 //    
 //    NSArray* words = [resultString componentsSeparatedByCharactersInSet:set];
 //    NSLog(@"words = %@", words);
     
-    return resultString.length <=10;
+    textField.text = resultString;
+    
+    return FALSE;
 }
 
 @end
