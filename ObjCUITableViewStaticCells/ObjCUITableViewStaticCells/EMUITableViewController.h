b0098f3cf3d1fab80c718eb33792b8502f970c98
ATOM27 <gek2395@mail.ru>
2016-07-28 00:58:03 +0300

    UITableView

---
+++ ObjCUITableViewStaticCells/ObjCUITableViewStaticCells/EMUITableViewController.h
@@ -0,0 +1,13 @@
+//
+//  EMUITableViewController.h
+//  ObjCUITableViewStaticCells
+//
+//  Created by Eugene Mekhedov on 28.07.16.
+//  Copyright © 2016 Eugene Mekhedov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EMUITableViewController : UITableViewController
+
+@end
---
+++ ObjCUITableViewStaticCells/ObjCUITableViewStaticCells/EMUITableViewController.m
@@ -0,0 +1,29 @@
+//
+//  EMUITableViewController.m
+//  ObjCUITableViewStaticCells
+//
+//  Created by Eugene Mekhedov on 28.07.16.
+//  Copyright © 2016 Eugene Mekhedov. All rights reserved.
+//
+
+#import "EMUITableViewController.h"
+
+@interface EMUITableViewController ()
+
+@end
+
+@implementation EMUITableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
