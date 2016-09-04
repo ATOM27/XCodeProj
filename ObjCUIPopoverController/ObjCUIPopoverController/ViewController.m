8641ab5afb551bf34468e7840ddc6b762bb7f1b9
ATOM27 <gek2395@mail.ru>
2016-09-05 02:44:37 +0300

    popover continue

--- ObjCUIPopoverController/ObjCUIPopoverController/ViewController.m
+++ ObjCUIPopoverController/ObjCUIPopoverController/ViewController.m
@@ -27,13 +27,48 @@
 
 #pragma mark - Actions
 
+-(void) showControllerAsModal:(UIViewController*) vc{
+   
+    [self presentViewController:vc
+                       animated:YES
+                     completion:^{
+                         dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+                             [self dismissViewControllerAnimated:YES completion:nil]; // dismiss is to close modal controller. We don't send "vc" to dissmis because on our controller can be obly one modal controller and "self" knows what controller it should close
+                         });
+                     }]; //presentViewController is like modal Action Segue
+}
+
 - (IBAction)actionAdd:(UIBarButtonItem *)sender {
     
     EMDetailsViewController* vc = [self.storyboard instantiateViewControllerWithIdentifier:@"EMDetailsViewController"];
     
-    UIPopoverController* popover = [[UIPopoverController alloc] initWithContentViewController:vc];
+    if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad){
+    
+        UIPopoverController* popover = [[UIPopoverController alloc] initWithContentViewController:vc];
+    
+        [popover presentPopoverFromBarButtonItem:sender
+                        permittedArrowDirections:UIPopoverArrowDirectionAny
+                                        animated:YES];
+    }else{
+        
+        [self showControllerAsModal:vc];
+    }
 }
 
 - (IBAction)actionPressMe:(UIButton *)sender {
+    
+    EMDetailsViewController* vc = [self.storyboard instantiateViewControllerWithIdentifier:@"EMDetailsViewController"];
+    
+    if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad){
+        
+        UIPopoverController* popover = [[UIPopoverController alloc] initWithContentViewController:vc];
+        
+//        [popover presentPopoverFromBarButtonItem:sender
+//                        permittedArrowDirections:UIPopoverArrowDirectionAny
+//                                        animated:YES];
+    }else{
+        
+        [self showControllerAsModal:vc];
+    }
 }
 @end
