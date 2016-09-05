1c603626d76ef1ff3fe1df9a55752819321db09d
ATOM27 <gek2395@mail.ru>
2016-09-06 01:18:11 +0300

    popover continue working

--- ObjCUIPopoverController/ObjCUIPopoverController/ViewController.m
+++ ObjCUIPopoverController/ObjCUIPopoverController/ViewController.m
@@ -38,17 +38,50 @@
                      }]; //presentViewController is like modal Action Segue
 }
 
+-(void) showController:(UIViewController*) vc inPopoverFromSender:(id) sender{
+    
+    //vc.modalPresentationStyle = UIModalPresentationPopover;
+    //vc.preferredContentSize = CGSizeMake(300, 300);
+    //[self presentViewController:vc animated:YES completion:nil];
+    
+    UINavigationController* navContr = [[UINavigationController alloc] initWithRootViewController:vc];
+    navContr.modalPresentationStyle = UIModalPresentationPopover;
+    
+    navContr.preferredContentSize = CGSizeMake(300, 300);
+    
+    [self presentViewController:navContr animated:YES completion:nil];
+    
+    UIPopoverPresentationController* popover = [navContr popoverPresentationController]; //[vc popoverPresentationController];
+    popover.permittedArrowDirections = UIPopoverArrowDirectionAny;
+    
+    if ([sender isKindOfClass:[UIBarButtonItem class]]){
+        
+        popover.barButtonItem = sender;
+    }else if ([sender isKindOfClass:[UIButton class]]){
+        
+        popover.sourceView = self.view;
+        
+        popover.sourceRect = CGRectMake(CGRectGetMidX([(UIButton*)sender frame]), CGRectGetMinY([(UIButton*)sender frame]), 0, 0);// so if you want to set x and y of your popover you need to make "sourceRect" with 0 size, but in "vc.preferredContentSize" need to make prefered size of your popover;
+        //UIPopoverController* popover = [[UIPopoverController alloc] initWithContentViewController:vc];
+        
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            [vc dismissViewControllerAnimated:YES completion:nil];
+        });
+    }
+}
+
 - (IBAction)actionAdd:(UIBarButtonItem *)sender {
     
     EMDetailsViewController* vc = [self.storyboard instantiateViewControllerWithIdentifier:@"EMDetailsViewController"];
     
     if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad){
     
-        UIPopoverController* popover = [[UIPopoverController alloc] initWithContentViewController:vc];
-    
-        [popover presentPopoverFromBarButtonItem:sender
-                        permittedArrowDirections:UIPopoverArrowDirectionAny
-                                        animated:YES];
+        //UIPopoverController* popover = [[UIPopoverController alloc] initWithContentViewController:vc]; // deprecated
+        
+        [self showController:vc inPopoverFromSender:sender];
+        //[popover presentPopoverFromBarButtonItem:sender
+          //              permittedArrowDirections:UIPopoverArrowDirectionAny
+            //                            animated:YES]; // deprecated
     }else{
         
         [self showControllerAsModal:vc];
@@ -61,11 +94,8 @@
     
     if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad){
         
-        UIPopoverController* popover = [[UIPopoverController alloc] initWithContentViewController:vc];
+        [self showController:vc inPopoverFromSender:sender];
         
-//        [popover presentPopoverFromBarButtonItem:sender
-//                        permittedArrowDirections:UIPopoverArrowDirectionAny
-//                                        animated:YES];
     }else{
         
         [self showControllerAsModal:vc];
